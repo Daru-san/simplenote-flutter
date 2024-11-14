a51a7b119d053b2117072ccc62692fe25816c359
Daru <mica.tui@proton.me>
2024-11-14 16:09:05 +0200

    app/pages: use yaru widgets on main page

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -3,6 +3,7 @@ import 'package:simplenote_flutter/api/data.dart';
 import 'package:simplenote_flutter/pages/home/home_page.dart';
 import 'package:simplenote_flutter/pages/settings/settings_page.dart';
 import 'package:simplenote_flutter/pages/tags/tag_page.dart';
+import 'package:yaru/yaru.dart';
 
 var userData = Data.newData();
 
@@ -15,65 +16,52 @@ class MainPage extends StatefulWidget {
   }
 }
 
-//TODO: Check if a user is logged in, if not, send them to log in
-void getData() async {
-  var data = userData.getData();
-  userData = await data;
-}
-
 class _MainPageState extends State<MainPage> {
   int _activePage = 0;
-  String _appTile = "Home";
-
   static const List<Widget> _pages = <Widget>[
     HomePage(),
     TagPage(),
     SettingsPage(),
   ];
 
+  static const List<String> _pageTitles = <String>[
+    "Home",
+    "Tags",
+    "Settings",
+  ];
+
+  static const List<IconData> _pageIcons = <IconData>[
+    YaruIcons.home,
+    YaruIcons.tag,
+    YaruIcons.settings,
+  ];
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_appTile),
+      appBar: YaruWindowTitleBar(
+        title: Text(_pageTitles[_activePage]),
+        isMinimizable: false,
+        isRestorable: false,
+        isClosable: false,
+        isMaximizable: false,
       ),
-      body: Row(
-        children: <Widget>[
-          NavigationRail(
-            selectedIndex: _activePage,
-            onDestinationSelected: (int index) {
-              setState(() {
-                _activePage = index;
-                _appTile = _pages[index].toString();
-              });
-            },
-            labelType: NavigationRailLabelType.selected,
-            destinations: const <NavigationRailDestination>[
-              NavigationRailDestination(
-                icon: Icon(Icons.dashboard_outlined),
-                selectedIcon: Icon(Icons.dashboard),
-                label: Text('Dashboard'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.tag_outlined),
-                selectedIcon: Icon(Icons.tag),
-                label: Text('Tags'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.settings_outlined),
-                selectedIcon: Icon(Icons.settings),
-                label: Text('Settings'),
-              ),
-            ],
-          ),
-          const VerticalDivider(width: 1, thickness: 1),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [_pages[_activePage]],
-            ),
-          )
-        ],
+      body: YaruMasterDetailPage(
+        length: _pages.length,
+        tileBuilder: (context, index, selected, avaiableWidth) {
+          return YaruMasterTile(
+            leading: Icon(_pageIcons[index]),
+            title: Text(_pageTitles[index]),
+          );
+        },
+        pageBuilder: (context, index) {
+          setState(() {
+            _activePage = index;
+          });
+          return Center(child: _pages[index]);
+        },
       ),
     );
   }

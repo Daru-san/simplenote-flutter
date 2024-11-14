49ba3b1432e19882ffc6a8a870aaffbe34118752
Daru <mica.tui@proton.me>
2024-11-14 12:12:01 +0200

    app/pages: update home page
    Adds a little bit more flesh to the home page.
    Also initializes note creation to that we can begin to actually test
    notes

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:simplenote_flutter/api/data.dart';
+import 'package:simplenote_flutter/note/note.dart';
+import 'package:simplenote_flutter/pages/note/note_page.dart';
+
+// final Data userData;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,8 +15,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Note> noteList = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Simplenote"),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: noteList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(noteList[index].title),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        NotePage(currentNote: noteList[index]),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Create a new note.",
+        onPressed: () {
+          var newNote = Note.newNote();
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => NotePage(currentNote: newNote),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }

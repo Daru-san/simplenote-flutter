77b6c035611900a536a2fe032f60ec516d256c9e
Daru <mica.tui@proton.me>
2024-11-14 12:07:34 +0200

    app/pages: init tags page

---
+++ lib/pages/tags/tag_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TagPage extends StatelessWidget {
+  const TagPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

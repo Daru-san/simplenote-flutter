dedb45800f48a689461c58fc1bc58ddbb3fd1474
Daru <mica.tui@proton.me>
2024-11-15 17:10:49 +0200

    app/main: remove rhttp client

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:rhttp/rhttp.dart';
 import 'package:simplenote_flutter/models/session/session.dart';
 import 'package:simplenote_flutter/pages/main/main_page.dart';
 import 'package:yaru/yaru.dart';
 
-RhttpClient httpClient = RhttpClient.createSync();
 Session currentSession = Session(email: "default");
 
 Future<void> main() async {
   await YaruWindowTitleBar.ensureInitialized();
-
-  Rhttp.init();
 
   currentSession.syncSimplenote();
 

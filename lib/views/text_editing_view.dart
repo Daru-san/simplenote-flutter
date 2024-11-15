241b73a4c351fa77496869d08f05fbc464630c4a
Daru <mica.tui@proton.me>
2024-11-15 22:56:48 +0200

    app/view/text: revert expand

--- lib/views/text_editing_view.dart
+++ lib/views/text_editing_view.dart
@@ -13,15 +13,17 @@ class TextEditingView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
       child: MarkdownAutoPreview(
+        enableToolBar: true,
         scrollController: scrollController,
         controller: textFieldController,
         emojiConvert: true,
         maxLines: null,
         textCapitalization: TextCapitalization.sentences,
         decoration: InputDecoration(),
-        toolbarBackground: Theme.of(context).cardColor,
         style: TextStyle(
           fontSize: 19,
           height: 1.5,

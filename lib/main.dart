0aea7f471a7d9b3dc74cd11b027e532b4c554f97
Daru <mica.tui@proton.me>
2024-11-15 23:02:25 +0200

    app/main: fix sqflite factory error

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:simplenote_flutter/models/database/database.dart';
 import 'package:simplenote_flutter/models/session/session.dart';
 import 'package:simplenote_flutter/pages/main/main_page.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:yaru/yaru.dart';
 
 var currentSession = Session.newSession();
@@ -9,6 +10,10 @@ var noteDB = DatabaseHelper.make();
 
 Future<void> main() async {
   await YaruWindowTitleBar.ensureInitialized();
+
+  sqfliteFfiInit();
+
+  databaseFactory = databaseFactoryFfi;
 
   // currentSession = await currentSession.syncSimplenote();
   noteDB.initDB();

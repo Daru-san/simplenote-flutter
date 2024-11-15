5d44e72f6e1db9c2f4e9995c165adbaa2fec61c7
Daru <mica.tui@proton.me>
2024-11-15 22:37:25 +0200

    app/page/home: get all notes from database

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simplenote_flutter/main.dart';
 import 'package:simplenote_flutter/models/note/note.dart';
 import 'package:simplenote_flutter/pages/note/note_page.dart';
 
@@ -14,81 +15,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<Note> noteList = [
-    Note(
-      key: 1.toString(),
-      content: '''
-      This is a body of text put into this special little note of mine
-      ''',
-      modifydate: DateTime.now(),
-      createdate: DateTime.now(),
-      systemtags: [],
-      tags: [],
-      syncnum: 1,
-      version: 1,
-      isDeleted: false,
-    ),
-    Note(
-      key: 2.toString(),
-      content: '''
-      This other body of text is really interesting
-      ''',
-      modifydate: DateTime.now(),
-      createdate: DateTime.now(),
-      systemtags: [],
-      tags: [],
-      syncnum: 1,
-      version: 1,
-      isDeleted: false,
-    )
-  ];
+  List<Note> noteList = [];
+
+  _getAllNotes() async {
+    noteList = await noteDB.getAllNotes() as List<Note>;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('All notes'),
-        backgroundColor: Theme.of(context).secondaryHeaderColor,
-        actions: [],
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: noteList.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(noteList[index].content.substring(0, 10)),
-              isThreeLine: true,
-              selectedTileColor: Theme.of(context).hoverColor,
-              tileColor: Theme.of(context).cardColor,
-              subtitle: Text("${noteList[index].content.substring(0, 40)}..."),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NotePage(),
-                    settings: RouteSettings(arguments: noteList[index]),
-                  ),
-                );
-              },
+    return FutureBuilder(
+      future: _getAllNotes(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
             );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: "Create a new note.",
-        onPressed: () {
-          var newNote = Note.newNote();
+          default:
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('All notes'),
+                backgroundColor: Theme.of(context).secondaryHeaderColor,
+                actions: [],
+              ),
+              body: Center(
+                child: ListView.builder(
+                  itemCount: noteList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(noteList[index].content.substring(0, 10)),
+                      isThreeLine: true,
+                      selectedTileColor: Theme.of(context).hoverColor,
+                      tileColor: Theme.of(context).cardColor,
+                      subtitle: Text(
+                          "${noteList[index].content.substring(0, 40)}..."),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NotePage(),
+                            settings: RouteSettings(arguments: noteList[index]),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                tooltip: "Create a new note.",
+                onPressed: () {
+                  var newNote = Note.newNote();
 
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => NotePage(),
-              settings: RouteSettings(arguments: newNote),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => NotePage(),
+                      settings: RouteSettings(arguments: newNote),
+                    ),
+                  );
+                },
+                child: const Icon(Icons.add),
+              ),
+            );
+        }
+      },
     );
   }
 }
